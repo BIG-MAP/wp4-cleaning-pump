946c2e61164c20be3e1fcbcfc2d984ade26eff14
Ihar Suvorau <ihar.suvorau@gmail.com>
2023-08-03 10:25:56 +0200

    Refactor the driver

--- cleaning_pump_driver/driver.py
+++ cleaning_pump_driver/driver.py
@@ -1,115 +1,96 @@
 import logging
 import os
 import time
-from enum import Enum
+from typing import Optional
 
 import PyCmdMessenger
-from pydantic import BaseModel, Field
 
 serial_port = os.environ.get("CLEANING_PUMP_SERIAL_PORT")
 
-pumpState = False
 
-arduino = PyCmdMessenger.ArduinoBoard(serial_port, baud_rate=115200, timeout=10)
+class ArduinoController:
+    def __init__(self, port: str) -> None:
+        self._busy = False
+        self._logger = logging.getLogger(__class__.__name__)
 
-# List of command names (and formats for their associated arguments). These must
-# be in the same order as in the sketch.
-commands = [
-    ["kWatchdog", "s"],
-    ["kAcknowledge", "s"],
-    ["kError", "s"],
-    ["kSetVoltage", "i"],
-    ["kStopPump", "s"],
-    ["kCyclePump", ""],
-]
+        _arduino = PyCmdMessenger.ArduinoBoard(port, baud_rate=115200, timeout=10)
+        # List of command names (and formats for their associated arguments).
+        # These must be in the same order as in the sketch.
+        commands = [
+            ["kWatchdog", "s"],
+            ["kAcknowledge", "s"],
+            ["kError", "s"],
+            ["kSetVoltage", "i"],
+            ["kStopPump", "s"],
+            ["kCyclePump", ""],
+        ]
+        # Initialize the messenger
+        self._messenger = PyCmdMessenger.CmdMessenger(_arduino, commands)
 
-# Initialize the messenger
-comm = PyCmdMessenger.CmdMessenger(arduino, commands)
-# Wait for arduino to come up
-msg = comm.receive()
-print(msg)
+        # Wait for arduino to come up
+        response = self._messenger.receive()
+        logging.info(f"Arduino says: {response}")
 
+    def _set_voltage(self, voltage: int):
+        """
+        Sets the voltage of the DAC controlling the pump. Range between 0 and 4096.
+        """
 
-def setVoltage(voltage: int) -> int:
-    """Function for setting the voltage of the DAC controlling the pump (0-4096)."""
+        self._messenger.send("kSetVoltage", voltage)
 
-    comm.send("kSetVoltage", voltage)
+        response = self._messenger.receive()
+        logging.info(response[1])
 
-    msg = comm.receive()
-    logging.info(msg[1])
+    def _stop_pump(self):
+        self._messenger.send("kStopPump")
 
-    return 0
+        response = self._messenger.receive()
+        logging.info(response[1])
 
+        self._busy = False
 
-def stopPump() -> int:
-    global pumpState
-    """Function for stopping the pump."""
+    def _cycle_pump(self):
+        """
+        Starts or stops the pump.
+        """
 
-    comm.send("kStopPump")
+        self._messenger.send("kCyclePump")
 
-    msg = comm.receive()
-    logging.info(msg[1])
+        response = self._messenger.receive()
+        logging.info(response[1])
 
-    pumpState = False
+        time.sleep(0.4)
 
-    return 0
+        self._busy = not self._busy
 
-
-def cyclePump() -> int:
-    global pumpState
-    """Function for starting or stopping the pump."""
-
-    comm.send("kCyclePump")
-
-    msg = comm.receive()
-    logging.info(msg[1])
-
-    time.sleep(0.4)
-
-    pumpState = not pumpState
-
-    return 0
+    def _is_busy(self) -> bool:
+        return self._busy
 
 
-def getPumpState() -> bool:
-    """Function for returning the pump state"""
-    global pumpState
+class CleaningPump(ArduinoController):
+    def __init__(self, port: str) -> None:
+        """
+        A diaphragm pump with a DAC to control the speed.
+        Speed is set by a voltage between 0 and 4096.
+        """
+        super().__init__(port)
+        self._default_speed = 1024
 
-    return pumpState
+    def start(self, speed: Optional[int] = None):
+        """
+        Starts the pump at a given speed, a range between 0 and 4096.
+        If no speed is given, the pump will start at the default speed.
+        """
+        if self._is_busy():
+            raise RuntimeError("Pump is already running")
 
+        self._set_voltage(speed or self._default_speed)
+        self._cycle_pump()
+        self._logger.info(f"Pump has started at {speed}")
 
-class Status(str, Enum):
-    stopped = "stopped"
-    running = "running"
+    def stop(self):
+        self._stop_pump()
+        self._logger.info("Pump has stopped")
 
-
-class PumpSettings(BaseModel):
-    speed: int = Field(0, title="The pump speed", ge=0, le=4095, example=1024)
-    run: bool = Field(False, title="Start the pump", description="Starts or stops the pump regardless of speed")
-
-
-class DiaphragmPump:
-    status: Status = Status.stopped
-    settings: PumpSettings = PumpSettings()
-
-    def startPump(self):
-        self.settings.run = True
-        self.status = Status.running
-        if getPumpState():
-            print("Pump already started")
-        else:
-            cyclePump()
-            print("Pump started")
-
-    def stopPump(self):
-        if getPumpState():
-            stopPump()
-            self.settings.run = False
-            self.settings.speed = 0
-            self.status = Status.stopped
-        print("Pump stopped")
-
-    def setSpeed(self, speed: int):
-        self.settings.speed = speed
-        setVoltage(speed)
-        print("Pump speed set to " + str(speed))
+    def is_busy(self) -> bool:
+        return self._is_busy()
